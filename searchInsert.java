493a96bfc744ecd208118ea0f1b0b9a39c3269e5
Vinh Nguyenz <danhsapmeta112@gmail.com>
2025-10-16 18:11:32 +0700

    Create searchInsert.java

---
+++ searchInsert.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int j;
+        for(int i=0;i< nums.length;i++)
+             {if (nums[i] == target) {return i;}
+               if( nums[i] > target) {return i;}}
+                     return nums.length;
+               
+        
+}
+}
